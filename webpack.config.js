cbf6de8e47936b9fcf7b6865e872bc37a370303b
Larry Olguin <iraqwarvet31@gmail.com>
2024-10-04 16:45:58 -0700

    initial add boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // apply to all JS files
+        exclude: /node_modules/, // exclude all files on node_modules
+        use: {
+          loader: 'babel-loader', // looks at .babelrc
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html', // create a template to start from
+    }),
+  ],
+  devServer: {
+    host: 'localhost', // where to run
+    historyApiFallback: true,
+    port: 3000, //given port to exec. app
+    open: true, // open new tab
+    hot: true, // Enable webpack's Hot Module Replacement
+  },
+  mode: 'production',
+};
